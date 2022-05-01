d984ae493e7fd37561e4ade7240c67dc5a49aa8d
sterashima78 <sterashima78@gmail.com>
2022-05-01 10:39:02 +0000

    abc002A

---
+++ abc/abc002A/src/main.rs
@@ -0,0 +1,19 @@
+// -*- coding:utf-8-unix -*-
+
+use proconio::input;
+
+// ABC002A
+
+fn main() {
+    input! {
+        n: u64,
+        m: u64,
+    }
+    let ans = if n > m {
+        n
+    } else {
+        m
+    };
+    println!("{}", ans);
+}
+
