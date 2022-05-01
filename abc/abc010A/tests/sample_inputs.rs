7950ce2b6866de682cc6ab269a98d0113052e52b
sterashima78 <sterashima78@gmail.com>
2022-05-01 11:34:48 +0000

    abc010A

---
+++ abc/abc010A/tests/sample_inputs.rs
@@ -0,0 +1,33 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"chokudai
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "chokudaipp\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(
+            r#"sanagi
+"#,
+        )
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "sanagipp\n");
+    assert!(output.stderr_str().is_empty());
+}
