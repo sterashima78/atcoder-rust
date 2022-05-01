f51e1512ef99152d65796fc35915c6c8bcd07750
sterashima78 <sterashima78@gmail.com>
2022-05-01 10:57:30 +0000

    abc004A

---
+++ abc/abc004A/src/main.rs
@@ -0,0 +1,13 @@
+// -*- coding:utf-8-unix -*-
+
+use proconio::input;
+
+// ABC004A
+
+fn main() {
+    input! {
+        n: u64,
+    }
+    println!("{}", 2 * n);
+}
+
---
+++ abc/abc004A/tests/sample_inputs.rs
@@ -0,0 +1,42 @@
+use cli_test_dir::*;
+
+const BIN: &'static str = "./main";
+
+#[test]
+fn sample1() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"1000
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "2000\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample2() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"1000000
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "2000000\n");
+    assert!(output.stderr_str().is_empty());
+}
+
+#[test]
+fn sample3() {
+    let testdir = TestDir::new(BIN, "");
+    let output = testdir
+        .cmd()
+        .output_with_stdin(r#"0
+"#)
+        .tee_output()
+        .expect_success();
+    assert_eq!(output.stdout_str(), "0\n");
+    assert!(output.stderr_str().is_empty());
+}
